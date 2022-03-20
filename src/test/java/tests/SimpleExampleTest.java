1eb87ce7ec85f62af3283bc3bbe489d971516b7c
yulia-gutorova <j.gutorova@gmail.com>
2022-03-20 12:29:48 +0100

    Initial commit

---
+++ src/test/java/tests/SimpleExampleTest.java
@@ -0,0 +1,90 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleExampleTest {
+
+    public static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    public static final String AUTOMATE_KEY = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    public static final String BS_URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public static RemoteWebDriver driver;
+    String view = "";
+
+    String url = "https://www.icabanken.se/lana/privatlan/hur-mycket-far-jag-lana/";
+
+    @DisplayName("WebPage url is right")
+    @Order(1)
+    @Test
+    public void testPageUrlIsRight() throws InterruptedException, MalformedURLException {
+
+        //mvn test -Dviewmode="web_chrome"
+
+        view = System.getProperty("viewmode");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        switch (view)
+        {
+            case "web_chrome":
+
+                caps.setCapability("os", "Windows");
+                caps.setCapability("os_version", "10");
+                caps.setCapability("browser", "Chrome");
+                caps.setCapability("browser_version", "98.0");
+                caps.setCapability("browserstack.local", "false");
+                caps.setCapability("browserstack.selenium_version", "3.14.0");
+
+                break;
+
+            case "mobile":
+
+                caps.setCapability("os_version", "10.0");
+                caps.setCapability("device", "Samsung Galaxy S20");
+                caps.setCapability("real_mobile", "true");
+                caps.setCapability("browserstack.local", "false");
+                caps.setCapability("browser", "chrome");
+
+                break;
+        }
+
+        WebDriver driver = new RemoteWebDriver(new URL(BS_URL), caps);
+
+        driver.get(url);
+
+        Actions actions = new Actions(driver);
+        WebElement cookiesButton = driver.findElement(By.xpath("//button[contains(text(), 'cookies')]"));
+        actions.moveToElement(cookiesButton);
+
+        Thread.sleep(3000);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(cookiesButton));
+        cookiesButton.click();
+
+        Thread.sleep(3000);
+
+        String currentURL = driver.getCurrentUrl();
+        Assertions.assertTrue(url.equalsIgnoreCase(currentURL));
+
+        driver.quit();
+    }
+
+}
